b8b9670935b86be878820eff64019fcd82958f50
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-04-16 16:16:06 +0000

    knowing about Variables

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const UserId = 101;
+let Usermail = "anjan@gmail.com"
+let UserPassword = "1234687"
+let isfollow;
+
+// UserId = 201; not Allowed  
+// console.log(UserName);
+
+/*don't use var for declaring variables 
+  because of issue in block scope and 
+  functional scope
+*/
+
+console.table([UserId, Usermail, UserPassword, isfollow])
