9a848cb63be80a4989210c2ad35a5e7e40402a16
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-04-16 17:24:47 +0000

    data_type

---
+++ basics/data_type.js
@@ -0,0 +1,18 @@
+"use strict" // trate all JS code as newer version
+
+alert ("hello moto") //we are using Nodejs not browser
+
+
+//Premitive Data Type
+
+/*
+number => 2 to power 53
+bigInt 
+string
+boolean=> true/false
+null => standalone value
+undefined => 
+symbol => unique
+*/
+
+// Non Premitive Data Types are Object
