68281fa4d7af8ec9a522736f9ea5ace275881039
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-04-17 18:34:26 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,42 @@
+// variables type check both way
+
+// let score = "100";
+// console.log(score);
+// console.log(typeof score);
+
+// console.log(typeof score);//normal way 
+
+// console.log(typeof(score));//here it is in a method form
+
+// let conversionNum = Number(score);
+// console.log(conversionNum);
+// console.log(typeof conversionNum);
+
+/* In JS conversion are a quite confusion there are
+
+"33" => 33 (easily convert string to number)
+"33rtb" => NaN(this string shows not a number after conversion done)
+
+*/
+
+
+
+// let dotValue=  0.00
+
+// let boolValue = Boolean(dotValue)
+
+// console.log(boolValue)
+
+
+
+// ******************** OPERATIONS************************
+
+ let x = 3
+ let u = ++x
+ let z = x 
+
+
+ 
+ console.log(u)
+ console.log(x)
+ console.log(z)
