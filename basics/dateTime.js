83bdf47d90616465ecdf4656bd31b7e888f4073c
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-06-09 17:29:11 +0000

    date and time

--- basics/dateTime.js
+++ basics/dateTime.js
@@ -1,4 +1,6 @@
-let myDate = new Date()
+//Dates are vast so 
+
+let myDate = new Date() //here date is a obj. 
 
 // Date is obj
 
@@ -15,9 +17,17 @@ let createAtDate = new Date(2024, 1, 23)
 //    Date Stamp
 
 let timeStamp = Date.now()
-console.log(timeStamp);
+// console.log(timeStamp);
 
-console.log(createAtDate.getTime());
+// console.log(createAtDate.getTime());
 
-console.log(Date.now()/10000);
+// console.log(Date.now()/10000);
 
+
+let newDate = new Date;
+// console.log(newDate);
+// console.log(newDate.getMonth());
+
+// newDate.toDateString('default',{
+//     weekday: "long"
+// })
