4b4b76d797a2f7850866f58b593ccf23c24b8c3b
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-06-10 13:11:21 +0000

    array and array

---
+++ basics_2/array.js
@@ -0,0 +1,59 @@
+const myArry = [0, 1, 2, 3, 4, 5];// this is array and 0, 1, 2, 3 are elements of the array 
+
+
+//Different Way to Declare the Array
+const newArr = new Array(1, 2, 3, 4 )// here in this way we doesnot used [] boxes for array decclaration
+// console.log(newArr [2]);
+
+//Array methods
+
+// myArry.push(8); //this push methods actually add element to the array
+// console.log(myArry);
+
+// myArry.pop(); //in this POP method its work is remove last element from the array 
+// console.log(myArry);
+
+// myArry.unshift(99);// this unshift method actually add element in the very 1st position of the Array. 
+//                     //so that this method basically changed original index values of an array. 
+//                     // for small array its nothing problem but problem came out huge number of data  
+// console.log(myArry);
+
+
+// myArry.shift(); // this method just remove very 1st element of an array
+// console.log(myArry);
+
+
+
+// console.log(myArry.includes(1));// in this method index ma send gareko number cha ki xaina vanera find out garcha
+
+// console.log(myArry.indexOf(3));
+
+
+// const newarry = myArry.join();// join method le bind gardincha array and string ma convert gar dincha 
+
+// console.log(myArry);
+// console.log(newarry);
+// console.log(typeof newarry);
+
+
+
+//Slice and Splice
+
+// console.log("A", myArry);
+
+// const arry =  myArry.slice(1,4)
+// console.log("B", myArry)
+// console.log(arry)
+
+
+// //splice method manupulate the original array 
+// const arry2 = myArry.splice(1,4)
+// console.log("C", myArry)
+// console.log(arry2)
+
+
+
+
+
+
+
