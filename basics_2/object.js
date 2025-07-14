8b9ca1529cb3608d52b4cf8259ab38b75ce0ed0b
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-07-14 18:15:34 +0000

    updated

--- basics_2/object.js
+++ basics_2/object.js
@@ -11,15 +11,41 @@ const mysmbl = Symbol("key1")
 //++++++++++++++++++++++ OBJECT LITERAL+++++++++++++++
 const user = {
     name: "Anjan",
-    age : 36,
-    email: "google@MediaList.com",
-    isLoggedin: "true",
-    LastPurchase: ["monday, wednasday"]
+    [mysmbl]: "myKey1",
+            age : 36,
+                email: "google@gmail.com",
+                    isLoggedin: "true",
+                        LastPurchase: ["monday, wednasday"]
 
-}
+                        }
+
+// How to get get values
 
 console.log(user.name) //normal way to ge the values of the OBJ
 
 console.log(user["name"]) // Recommendate way to get the VALUES of the OBJ
 
+console.log(typeof user[mysmbl])
 
+// if we want to add SYMBOL inside the OBJECT it should adding in the [] 
+
+
+
+
+// How to Change the value of a object that key have
+
+// Simply, 
+user.name = "Robina"
+
+//console.log(user["name"])
+// console.log(user)
+
+
+
+//how to freeze the value in a obj, Simply
+
+Object.freeze(user);
+
+user.name = "Sunny"
+
+console.log(user)
