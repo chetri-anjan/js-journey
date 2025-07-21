2d11c4fc8c3c489b5df15a1758440d02d7679501
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-07-21 18:16:53 +0000

    adding the content of var, let and const

--- basics/variables.js
+++ basics/variables.js
@@ -12,3 +12,49 @@ let isfollow;
 */
 
 console.table([UserId, Usermail, UserPassword, isfollow])
+
+
+
+
+//+++++++++++++++ VAR ++++++++++++++++++++++++
+
+//var are function-scoped or globally-scoped. 
+
+//This means they are accessible throughout the entire function in which they are declared, 
+// or globally if declared outside of any function.
+
+var y = 77
+console.log(y)
+
+y = 78
+console.log(y)
+
+
+//+++++++++++++++ LET +++++++++++++++++++
+
+//block-scoped, meaning they are only accessible within the block.{}
+
+//let variables can be reassigned but cannot be re-declared within the same block scope. cannot redeclare 
+// like "let mynum", after declaring the variable
+
+let  mynum = 33;
+console.log(mynum)
+
+mynum = 44;
+console.log(mynum)
+
+
+
+//+++++++++++++++++++++++++ CONST +++++++++++++++++++++++++
+
+// const are also block-scoped, similar to let. {}
+
+//const variables cannot be reassigned after their initial assignment
+
+const x = 55
+console.log(x)
+
+// x = 77   Here showing error
+// console.log(x)
+
+
