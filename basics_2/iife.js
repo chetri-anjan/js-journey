dc7882010b89353508657f8d831d96a5650a4468
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-07-28 10:03:56 +0000

    About iife

---
+++ basics_2/iife.js
@@ -0,0 +1,51 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
+
+// Function create gareko immediately nai execute garni bela 
+
+//Global Scope le polution create garcha function horu ma some time, 
+// ani atleast yo function ma polute nahaus vanera iife Used garincha
+
+// OR global scope ko variable or junsukai declaration cha global scope ma
+// tesko pollution lai remove garna lai IIFE use garincha
+
+
+
+// function coffee(){
+//     console.log(`DB Connected`)
+// }
+// coffee()
+
+
+//++++++++++++++++++++  IIFE  +++++++++++
+
+//  So Function lai rap garcha () yesma ani execute ni gardin cha tespachi using this ()
+
+//Below Example 
+
+//Named IIFE
+(function coffee(){
+    console.log(`DB Connected`)
+})();
+
+
+
+//UnNamed IIFE
+
+(() => {
+    console.log(`DB Second Connected`)
+})();
+
+
+//console.log le variable expecte garcha vane yesari ni parametter pass garcha 
+//parameter pass garni IIFE
+
+
+((username)=>{
+    console.log(`Welcome back to the Database!!, ${username}`)
+})("anjan")
+
+
+
+//duita IIFE lekhni vaye vane after execution 1st IIFE ; add garera end garna parcha execution lai natra error aauncha
+
+
