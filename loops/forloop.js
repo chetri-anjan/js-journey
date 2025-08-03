296c555c8033199ff3ff31a8673963656e128f70
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-08-03 18:59:38 +0000

    for loop context

---
+++ loops/forloop.js
@@ -0,0 +1,63 @@
+
+//++++++++++++++++++++++++++ BREAK +++++++++++++++++++++++++++++
+
+// for (let index = 0; index <= 20; index++) {
+//     const element = index;
+
+//     if(index == 5)
+//     {
+//         console.log(`5 is detected `)
+//         break; // BREAK KEY WORD USE GARNI BITTIKAI CONDITION MEET VAKO PACHI STOP VOI DINCHA 
+//     }
+
+//     console.log(`prinitng the values of ${element}`)
+    
+// }
+
+
+
+// +++++++++++++++++++++++++ CONTINUE ++++++++++++++++++++++++++++++++
+
+// for (let index = 0; index <= 20; index++) {
+//     const element = index;
+
+//     if(index == 5)
+//     {
+//         console.log(`5 is detected `)
+//         continue; // CONTINUE LE ONE TIME CHANCE DINCHA PRINT GARNA BUT 1 CHOTI SKIP GARE AARU PRINT GARCHA
+//     }
+
+//     console.log(`prinitng the values of ${element}`)
+    
+// }
+
+
+
+// +++++++++++++++++++++++ ARRAY ++++++++++++++++++++++++++++++++
+
+// let heros = ['Shaktima', 'Junior G', 'Hattori', 'Motu Patlu']
+
+// for (let index = 0; index < heros.length; index++) {
+//     const element = heros[index]; // array lai print garni bela index halnai parcha hai
+//     // Ani index value jaile ni small nai huncha array ko length vanda 
+//     console.log(element)
+    
+// }
+
+
+//+++++++++++++++++ MULTIPILCATION TABLE USING LOOP  +++++++++++++++++++++++
+
+for (let index = 1; index <= 10; index++) {
+
+    // console.log(`print the values OUTTER LOOP ${index}`)
+
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`print the values INNER LOOP ${j}`)
+        
+        // console.log(`${index}  * ${j} = ${index * j} `)
+    }
+    
+
+}
+
+
