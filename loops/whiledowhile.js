96869b548c6a30c08f3abfeb44867763b8cc5543
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-08-03 19:19:26 +0000

    do while and while

---
+++ loops/whiledowhile.js
@@ -0,0 +1,28 @@
+
+let heros = ['Shaktima', 'Junior G', 'Hattori', 'Motu Patlu']
+
+let index = 0
+
+while (index < heros.length) {
+    // console.log(`print the array named Heros: ${heros[index]}`);
+    
+    index++
+}
+
+
+const nums = 10
+
+while (nums > 10) {
+    console.log(nums)
+}
+
+
+
+
+
+let num = 10
+
+do {
+    console.log(`DO while loop vale: ${num}`)
+    
+} while (num > 10 );
