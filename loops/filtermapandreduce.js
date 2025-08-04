326d8082287beedc9cdd308c3a9072218db3fce5
Anjan chetri <142468177+chetri-anjan@users.noreply.github.com>
2025-08-04 18:05:14 +0000

    added

---
+++ loops/filtermapandreduce.js
@@ -0,0 +1,20 @@
+// const coding = ["java", "python", "php", "C# ", "jS" ]
+
+// const myCoding = coding.forEach( (item) => {
+//     console.log(item)
+
+//     return item
+// })
+// console.log(myCoding)
+
+
+
+// Yesko Output undefined aauncha so One thing clear that FOR EACH le value return nagarni raicha 
+
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.filter(()+>
+)
